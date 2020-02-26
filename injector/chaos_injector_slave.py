ad3ce63e608d725fa84ad6164003342ce4f916ab
amos-baron <amosbar1000@gmail.com>
2020-02-26 14:29:19 +0200

    added get info and get fault info function

--- injector/chaos_injector_slave.py
+++ injector/chaos_injector_slave.py
@@ -8,23 +8,50 @@ class InjectionSlave():
         self.db_api_url = "http://{}:{}".format(self.db_ip,self.db_api_port)
 
     def initiate_fault(self,dns,fault):
-        self.orchestrate_injection(dns,fault)
+        return self.orchestrate_injection(dns,fault)
 
     def orchestrate_injection(self,dns,fault):
         target_info , fault_info = self.get_info(dns,fault)
         built_script = self.build_script(target_info,fault_info)
-        injection_logs = self.inject_script()
-        self.send_result(injection_logs)
+        #injection_logs = self.inject_script()
+        #self.send_result(injection_logs)
+
+        return target_info , fault_info
 
     def get_info(self,dns,fault):
         db_server_api_url = "{}/{}/{}".format(self.db_api_url,"server",dns)
         db_fault_api_url = "{}/{}/{}".format(self.db_api_url, "fault", fault)
-        server_info = requests.get(db_server_api_url)
-        fault_info = requests.get(db_fault_api_url)
+
+        fault_info = self.get_fault_info(db_fault_api_url)
+
+        # Getting server and fault info and turning them into json
+        server_info = requests.get(db_server_api_url).json()
+
         return server_info, fault_info
+
+
+    def get_fault_info(self,db_fault_api_url):
+        fault_info = requests.get(db_fault_api_url).json()
+        probes = fault_info["probes"]
+        methods = fault_info["methods"]
+        rollbacks = fault_info["rollbacks"]
+
+        fault_structure = {'probes' : probes , 'methods' : methods , 'rollbacks' : rollbacks}
+
+        # fault_section can be the probes/methods/rollbacks part of the fault
+        for fault_section in fault_structure.keys():
+            fault_section_parts = []
+            # section_part refers to a specific part of the probes/methods/rollbacks
+            for section_part in fault_structure[fault_section]:
+                section_part_info = requests.get("{}/{}/{}".format(self.db_api_url,fault_section,section_part)).json()
+                fault_section_parts.append(section_part_info)
+            fault_structure[fault_section] = fault_section_parts
+        return fault_structure
+
 
     def build_script(self,target_info,fault_info):
         print(target_info,fault_info)
+        return "working"
     def inject_script(self):
         pass
 
