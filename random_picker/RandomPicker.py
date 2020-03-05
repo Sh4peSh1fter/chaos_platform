c7130c28cb0a235ac26846b8f36d96cb320029e7
mike <mike>
2020-03-05 19:11:14 +0200

    adding faults route

--- random_picker/RandomPicker.py
+++ random_picker/RandomPicker.py
@@ -7,7 +7,7 @@ import os
 
 
 db_port = os.environ.get("API_PORT", 5000)
-mongo_host = os.environ.get("API_DB_HOST", "104.41.134.19")
+mongo_host = os.environ.get("API_DB_HOST", "127.0.0.1")
 db_host = "http://{0}:{1}".format(mongo_host, db_port)
 
 class Picker:
@@ -23,6 +23,21 @@ class Picker:
 
     def show_data(self):
         return json.dumps({"Interval": self.pick_interval, "Group to pick": self.group_pick}, indent=2)
+
+    def get_fault(self, server):
+        response = requests.get("{0}/fault".format(db_host))
+        filtered_fault_list = []
+        # Check if request succeeded
+        if "200" in str(response):
+            all_faults = json.loads(response.content.decode("utf-8"))["result"]
+            for fault in all_faults:
+                for server_group in server["groups"]:
+                    if fault["active"]:
+                        if server_group in fault["targets"]:
+                            filtered_fault_list.append(fault)
+                            break
+
+        return random.choice(filtered_fault_list)
 
     def get_server(self, new_group = "all"):
         response = requests.get("{0}/server".format(db_host))
@@ -45,18 +60,21 @@ class Picker:
 app = Flask(__name__)
 the_picker = Picker()
 
+
 @app.route('/get-server', methods=['POST'])
 def get_server():
-    print("request.get_json() = ", request.get_json())
     json_object = request.get_json()
-    print("json_object = ", json_object)
     try:
         group = json_object["group"]
-        print(group)
     except KeyError:
         return "group is a required parameter", 400
     return the_picker.get_server(group), 200
 
+@app.route('/get-fault', methods=['POST'])
+def get_fault():
+    json_object = request.get_json()
+
+    return the_picker.get_fault(json_object), 200
 
 @app.route('/pick-data',methods=['GET'])
 def get_picker_data():
