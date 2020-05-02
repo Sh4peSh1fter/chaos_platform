25e263cbf69d660b9d5d359055fa0232547b066e
amos-baron <amosbar1000@gmail.com>
2020-05-02 15:53:35 +0300

    added input validation to file config

--- cli/file_config.py
+++ cli/file_config.py
@@ -87,14 +87,16 @@ def get_data_from_json_file(file_path, varible_name):
 def add_data_to_json_file(file_path, varible_name,varible_value):
     validate_home_dir(chaos_home_dir)
     with open(file_path,'w+') as json_file :
-        #file_data  = json.load(json_file)
-        #file_data[varible_name] = varible_value
+        try :
+            file_data  = json.load(json_file)
+            file_data[varible_name] = varible_value
+        except :
+            file_data = {varible_name : varible_value}
 
         # Rewind to the start of the file
-        #json_file.seek(0)
-        #json.dump(file_data, json_file)
-        ## Truncate in case new data is smaller than old data
-        #file_data.truncate()
-        pass
+        json_file.seek(0)
+        json.dump(file_data, json_file)
+        # Truncate in case new data is smaller than old data
+        file_data.truncate()
 
 
