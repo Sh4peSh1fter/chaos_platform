32a6f5dae71775ca9c2a5732784a7130bf03290f
amos-baron <amosbar1000@gmail.com>
2020-05-05 13:02:52 +0300

    fixed examples

--- probe_examples/linux_httpd_is_running.py
+++ probe_examples/linux_httpd_is_running.py
@@ -1,18 +1,14 @@
 import os
-import argparse
-from pathlib import Path
 
-
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('-dns', action="store", dest="dns")
-
-def file_doesnt_exist(file_name):
-    file_exists = os.path.exists(file_name)
-    if file_exists == False :
+def get_service_status(service_name):
+    service_status = os.system("service {} status".format(service_name))
+    if service_status == 0:
         return True
-    else :
+    else:
         return False
 
+
 if __name__ == '__main__':
-    home_dir = str(Path.home())
-    file_doesnt_exist("{}/{}".format(home_dir,"fault_test"))
+    get_service_status("httpd")
+
+
